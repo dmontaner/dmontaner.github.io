2a9e9b50f59229aa2e6aed74f57b12d1b85d7973
dmontaner <david.montaner@gmail.com>
2017-02-01 09:05:35 +0000

    panelappR

---
+++ cv_long/z_make.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# z_make.sh
+# 2016-09-08 david.montaner@gmail.com
+# compile my latex cv
+
+## clear files
+rm logs/*
+rm *.aux
+rm *.log
+rm *.out
+rm *.pdf
+
+## compile
+xelatex david_montaner_cv_long.tex
+
+## keep logs
+mv *.aux logs/
+mv *.log logs/
+mv *.out logs/
